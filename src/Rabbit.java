1b88f2cf9a3fe8041ed15f1c9643b454ce908e43
Victor Memborg <75667343+victormemborg@users.noreply.github.com>
2023-11-21 17:51:43 +0100

    rabbits somewhat implemented
    - K1-2a: Complete
    - K1-2b: Complete
    - K1-2c: Not implemented
    - K1-2d: Age is implemented, but the energy level needs to be implemented
    - K1-2e: Not implemented
    - K1-2f: Rabbits can dig holes, but they are not able to share a hole yet.
    - K1-2g: Complete

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,15 +1,66 @@
-import itumulator.world.World;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.awt.Color;
+
+import itumulator.executable.DisplayInformation;
+import itumulator.executable.DynamicDisplayInformationProvider;
+import itumulator.simulator.*;
 import itumulator.world.*;
 
-import java.util.Random;
 
-import itumulator.simulator.*;
+public class Rabbit implements Actor, DynamicDisplayInformationProvider{
+    int age = 0;
+    int foodLevel = 19; //it gets deleted one tick before the 2nd night, if it has not found food.
+    Burrow burrow = null;
+    Location loc;
 
-public class Rabbit implements Actor{
     @Override
     public void act(World world) {
-        Random ran = new Random();
-        Location l = new Location(ran.nextInt(5), ran.nextInt(5));
-        world.move(this, l);
+        System.out.println(foodLevel);
+        if(!world.getEmptySurroundingTiles().isEmpty() && !world.isNight()) {
+            world.move(this, getEmptyRandomLocations(world));
+            foodLevel--;
+            if(foodLevel <= 0) {
+                die(world);
+            }
+            if(burrow == null) {
+                digHole(world);
+            }    
+        } 
+        if(world.isNight()) {
+            age += 1;
+            if(burrow != null) {
+                world.move(this, loc);
+            }
+        }
     }
+    
+    public void die(World world) {
+        world.delete(this);
+    }
+
+    public void digHole(World world) {
+        loc = world.getLocation(this);
+        burrow = new Burrow();
+        world.setTile(loc, burrow);
+    }
+
+    @Override
+    public DisplayInformation getInformation() {
+        return new DisplayInformation(Color.DARK_GRAY, "rabbit-small");
+    }
+
+    public Location getEmptyRandomLocations(World world) {
+        Random r = new Random();
+        Set<Location> neighbours = world.getEmptySurroundingTiles();
+        System.out.println(neighbours.size());
+        if (neighbours.isEmpty()) {
+            return null; 
+        }
+        List<Location> list = new ArrayList<>(neighbours);
+        return list.get(r.nextInt(list.size())); 
+    }    
+    
 }
