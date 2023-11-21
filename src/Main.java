492f604d599aa8570a5c9f4ece33ec10f39a4e54
21Philip <72072928+21Philip@users.noreply.github.com>
2023-11-21 17:41:01 +0100

    Fixet main

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         createWorld();
+        
     }
 
     /*
@@ -18,31 +19,34 @@ public class Main {
     */
     private static void createWorld() {
         try {
-            Scanner s = new Scanner(new File("data/t1-1a.txt"));
+            Scanner s = new Scanner(new File("data/test.txt"));
             int size = Integer.parseInt(s.nextLine());
             Program p = new Program(size, 800, 500);
             World world = p.getWorld();
 
-            while (s.hasNextLine()) {
+            while (s.hasNext()) {
                 //Lav object af klasse
                 String input = s.next();
                 String class_name = input.substring(0, 1).toUpperCase() + input.substring(1, input.length());
                 Class<?> class_type = Class.forName(class_name);
-                Object obj = class_type.getDeclaredConstructor().newInstance();
                 //lav x instancer i world
                 Random ran = new Random();
-                for (int i = 0 ; i < Integer.parseInt(s.next()) ; i++) {
+                int input2 = Integer.parseInt(s.next());
+                for (int i = 0 ; i < input2 ; i++) {
                     Location l = new Location(ran.nextInt(size), ran.nextInt(size));
-                    world.setTile(l, obj);
+                    world.setTile(l, class_type.getDeclaredConstructor().newInstance());
                 }
             }
-
+            s.close();
+            p.show();
+            p.run();
+            
         } catch (FileNotFoundException fnfe) {
             System.out.println(fnfe.getMessage());
         } catch (ClassNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
         }
     }
 }
