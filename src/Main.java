8241823354d9adefcd77142e5d893c2ef98f1c71
21Philip <72072928+21Philip@users.noreply.github.com>
2023-11-23 01:44:44 +0100

    dårlig main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import itumulator.executable.Program;
 import itumulator.world.Location;
@@ -10,46 +12,80 @@ import itumulator.world.World;
 public class Main {
 
     public static void main(String[] args) {
-        createWorld();
+        try {
+            createWorldFromFile("data/test.txt");
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Check path!");
+        }
         
     }
 
     /*
      dadadaaw
     */
-    private static void createWorld() {
-        try {
-            Scanner s = new Scanner(new File("data/test.txt"));
-            int size = Integer.parseInt(s.nextLine());
-            Program p = new Program(size, 800, 500);
-            World world = p.getWorld();
+    private static void createWorldFromFile(String path) throws FileNotFoundException{
+        Scanner s = new Scanner(new File(path));
+        int size = Integer.parseInt(s.nextLine());
+        Program p = new Program(size, 800, 500);
+        World world = p.getWorld();
+        ArrayList<Location> loclistNonBlock = getAllLocations(size);
+        ArrayList<Location> loclistBlock = getAllLocations(size);
 
-            while (s.hasNext()) {
-                //Lav object af klasse
-                String input = s.next();
-                String class_name = input.substring(0, 1).toUpperCase() + input.substring(1, input.length());
-                Class<?> class_type = Class.forName(class_name);
-                //lav x instancer i world
-                Random ran = new Random();
-                int input2 = Integer.parseInt(s.next());
-                for (int i = 0 ; i < input2 ; i++) {
-                    Location l = new Location(ran.nextInt(size), ran.nextInt(size));
-                    while (!world.isTileEmpty(l) || world.containsNonBlocking(l)) {
-                        l = new Location(ran.nextInt(size), ran.nextInt(size));
+        while (s.hasNext()) {
+            try {
+                //get class from input
+                Class<?> clazz = getClassFromInput(s);
+                //create specified number of instances
+                int antal = Integer.parseInt(s.next());
+                for (int i = 0 ; i < antal ; i++) {
+                    if (Arrays.toString(clazz.getInterfaces()).contains("NonBlocking")) {
+                        setRandomTileFromList(world, loclistNonBlock, clazz);
+                    } else {
+                        setRandomTileFromList(world, loclistBlock, clazz);
                     }
-                    world.setTile(l, class_type.getDeclaredConstructor().newInstance());
                 }
+            } catch (Exception e) {
+                System.out.println("Fejl i input, skipper linje");
             }
-            s.close();
-            p.show();
-            p.run();
-            
-        } catch (FileNotFoundException fnfe) {
-            System.out.println(fnfe.getMessage());
+        }
+        s.close();
+        p.show();
+        p.run();
+
+    }
+
+    private static ArrayList<Location> getAllLocations(int size) {
+        ArrayList<Location> loclist = new ArrayList<>();
+        for (int x = 0 ; x < size ; x++) {
+            for (int y = 0 ; y < size ; y++) {
+                loclist.add(new Location(x, y));
+            }
+        }
+        return loclist;
+    }
+
+    private static Class<?> getClassFromInput(Scanner s) {
+        try {
+            String input = s.next();
+            String class_name = input.substring(0, 1).toUpperCase() + input.substring(1, input.length());
+            Class<?> class_type = Class.forName(class_name);
+            return class_type;
         } catch (ClassNotFoundException cnfe) {
             System.out.println(cnfe.getMessage());
-        } catch (Exception e) {
-            System.out.println(e.getClass());
+            return null;
         }
+
+    }
+
+    private static void setRandomTileFromList(World world, ArrayList<Location> loclist, Class<?> class_type) {
+        try {
+            Random ran = new Random();
+            int r = ran.nextInt(loclist.size());
+            world.setTile(loclist.get(r), class_type.getDeclaredConstructor().newInstance());
+            loclist.remove(r);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " Fejl i setTileFromLocList(). Skipper dette step");
+        }
+
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,4 +1,88 @@
-public class Test {
-    Object k =wordl.getNonblocking(l)
-    world.delter(k)
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+
+public class test {
+
+    public static void main(String[] args) {
+        try {
+            createWorldFromFile("data/test.txt");
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Check path!");
+        }
+        
+    }
+
+    /*
+     dadadaaw
+    */
+    private static void createWorldFromFile(String path) throws FileNotFoundException{
+        Scanner s = new Scanner(new File(path));
+        int size = Integer.parseInt(s.nextLine());
+        Program p = new Program(size, 800, 500);
+        World world = p.getWorld();
+        ArrayList<Location> loclistNonBlock = getAllLocations(size);
+        ArrayList<Location> loclistBlock = getAllLocations(size);
+
+        while (s.hasNext()) {
+            try {
+                String input = s.next();
+                String class_name = input.substring(0, 1).toUpperCase() + input.substring(1, input.length());
+                Class<?> class_type = Class.forName(class_name);
+
+                int input2 = Integer.parseInt(s.next());
+                for (int i = 0 ; i < input2 ; i++) {
+                    if (Arrays.toString(class_type.getInterfaces()).contains("NonBlocking")) {
+                        setTileFromLocList(world, loclistNonBlock, class_type);
+                    } else {
+                        setTileFromLocList(world, loclistBlock, class_type);
+                    }
+                }
+
+            } catch (ClassNotFoundException cnfe) {
+                System.out.println(cnfe.getMessage());
+            }
+        }
+        s.close();
+        p.show();
+        p.run();
+        
+        /* 
+        } catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe.getMessage());
+        */
+    }
+
+    private static ArrayList<Location> getAllLocations(int size) {
+        ArrayList<Location> loclist = new ArrayList<>();
+        for (int x = 0 ; x < size ; x++) {
+            for (int y = 0 ; y < size ; y++) {
+                loclist.add(new Location(x, y));
+            }
+        }
+        return loclist;
+    }
+
+    private static Class<?> getClassTypeFromInput(Scanner s) {
+
+    }
+
+    private static void setTileFromLocList(World world, ArrayList<Location> loclist, Class<?> class_type) {
+        try {
+            Random ran = new Random();
+            int r = ran.nextInt(loclist.size());
+            world.setTile(loclist.get(r), class_type.getDeclaredConstructor().newInstance());
+            loclist.remove(r);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + " Fejl i setTileFromLocList(). Skipper dette step");
+        }
+
+    }
 }
