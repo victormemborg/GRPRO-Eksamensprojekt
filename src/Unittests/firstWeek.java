afa65abdd77f4665642ef6f448bfbd6f99c89991
Victor Memborg <75667343+victormemborg@users.noreply.github.com>
2023-12-08 20:03:06 +0100

    rest of wolf behaviour
    Week 1: 100%
    Week 2: 75%
    Week 3: 100%

--- src/Unittests/firstWeek.java
+++ src/Unittests/firstWeek.java
@@ -169,7 +169,7 @@ public class firstWeek {
         World world = p.getWorld();
         Rabbit rabbit = new Rabbit(world);
         world.setTile(new Location(0, 0), rabbit);
-        rabbit.createHome();
+        rabbit.createBurrow();
         Assertions.assertTrue(rabbit.getHome() != null); // if the rabbit has dug a burrow, it should have a home
     }
 
@@ -180,7 +180,7 @@ public class firstWeek {
         World world = p.getWorld();
         Rabbit rabbit = new Rabbit(world);
         world.setTile(new Location(0, 0), rabbit);
-        rabbit.createHome();
+        rabbit.createBurrow();
         world.setNight();
         p.simulate();
         int totalRabbit = 0;
